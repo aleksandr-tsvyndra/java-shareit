582c3479232ce07a6bd8cbcbb4f3cb8f4d14407c
Aleksandr Tsvyndra <aleks.tsvindra@gmail.com>
2025-08-17 01:27:52 +0300

    Реализовать модели данных: Booking, Item, ItemRequest и User

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.Instant;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class Booking {
+    private Long id;
+    private Instant start;
+    private Instant end;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
